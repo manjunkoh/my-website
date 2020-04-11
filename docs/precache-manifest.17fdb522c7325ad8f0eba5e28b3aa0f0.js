048cec67e36742d3b2a235d074fd75045417dd75
manjunkoh <manjunn12@gmail.com>
2020-04-11 15:10:43 +0900

    website successfully deployed

---
+++ docs/precache-manifest.17fdb522c7325ad8f0eba5e28b3aa0f0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894012e3aaa1b7a9e02fac8d2af13ce4",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "729ac88291314e5f96c0",
+    "url": "/my-website/static/css/2.5706590b.chunk.css"
+  },
+  {
+    "revision": "2f14c880e2b6264d90aa",
+    "url": "/my-website/static/css/main.90ac44fe.chunk.css"
+  },
+  {
+    "revision": "729ac88291314e5f96c0",
+    "url": "/my-website/static/js/2.f081cc77.chunk.js"
+  },
+  {
+    "revision": "312687650ecf95cf2205ba748f37e6e4",
+    "url": "/my-website/static/js/2.f081cc77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f14c880e2b6264d90aa",
+    "url": "/my-website/static/js/main.cedc9a3b.chunk.js"
+  },
+  {
+    "revision": "7d60f9870cc305540b02",
+    "url": "/my-website/static/js/runtime-main.4444a555.js"
+  },
+  {
+    "revision": "14b113574329f6898775182018d65f47",
+    "url": "/my-website/static/media/background.14b11357.jpg"
+  },
+  {
+    "revision": "9c916a05953d84ea6e64c551b3ef6a8e",
+    "url": "/my-website/static/media/classify_ships.9c916a05.png"
+  },
+  {
+    "revision": "e20baa6c488dc8d93c8057bedb3fe24d",
+    "url": "/my-website/static/media/cubsgame.e20baa6c.jpg"
+  },
+  {
+    "revision": "75bc029a19ae6d11fad0294fd12b1c1b",
+    "url": "/my-website/static/media/default_pic.75bc029a.jpg"
+  },
+  {
+    "revision": "2e779b259d6245d5cfdaa9280e4213c7",
+    "url": "/my-website/static/media/denver_main.2e779b25.jpg"
+  },
+  {
+    "revision": "7f1fcc4a5c1c4168a90015a11cd19650",
+    "url": "/my-website/static/media/guitarheropic.7f1fcc4a.png"
+  },
+  {
+    "revision": "4650f27061d3c27bc2da4f54dcac653d",
+    "url": "/my-website/static/media/me_and_merl.4650f270.jpg"
+  },
+  {
+    "revision": "d80a3f82e9b5e7f1ac9b199cb57f8948",
+    "url": "/my-website/static/media/mips_dis.d80a3f82.png"
+  },
+  {
+    "revision": "758aed4a1323f0bd9d904414186083e4",
+    "url": "/my-website/static/media/mywebsite.758aed4a.png"
+  },
+  {
+    "revision": "382209e0a3ddfcf75f8dfdca2a1d3902",
+    "url": "/my-website/static/media/newyears_boys.382209e0.jpg"
+  },
+  {
+    "revision": "6d0bdc181e23280c775cc760d2c52efa",
+    "url": "/my-website/static/media/nola_group.6d0bdc18.jpg"
+  },
+  {
+    "revision": "52e73076e77220a7c4634d306749a3b6",
+    "url": "/my-website/static/media/pf_example.52e73076.png"
+  },
+  {
+    "revision": "0f4401fb924bd207a8f36f6dc862ed4e",
+    "url": "/my-website/static/media/plotscript_example.0f4401fb.png"
+  },
+  {
+    "revision": "6fb2796d1198964f6ffc8cbbe503bbae",
+    "url": "/my-website/static/media/tailgate.6fb2796d.png"
+  },
+  {
+    "revision": "00b972c72797aced24f7818eef9dda3f",
+    "url": "/my-website/static/media/vawter_five.00b972c7.jpg"
+  },
+  {
+    "revision": "1eab8e2ed74e5b143c667cd80238dd1a",
+    "url": "/my-website/static/media/yosemite_entrance.1eab8e2e.jpg"
+  },
+  {
+    "revision": "b01c5701148d7f10940f544508dab413",
+    "url": "/my-website/static/media/yosemite_halfdome.b01c5701.jpg"
+  },
+  {
+    "revision": "00ebc01a1659b369a47861c78b0f8870",
+    "url": "/my-website/static/media/yosemite_main.00ebc01a.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b330dd1eb3baffe40ee75c411ac4f34a.js"
+  "/my-website/precache-manifest.17fdb522c7325ad8f0eba5e28b3aa0f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
