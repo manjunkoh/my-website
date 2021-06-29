0d6977ae9ab6f35ec5201c7de5a6f703123c551e
manjunkoh <manjunn12@gmail.com>
2021-06-29 22:47:42 +0900

    deploying the website on docs folder

---
+++ docs/precache-manifest.7d73b75dd22bf84b2e24813d1c4068a7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d867568edcb2aed07eba0aea0c09d4",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "bc769ad321184d3c6f58",
+    "url": "/my-website/static/css/2.5706590b.chunk.css"
+  },
+  {
+    "revision": "3f0fc8df14c5ec529e09",
+    "url": "/my-website/static/css/main.90ac44fe.chunk.css"
+  },
+  {
+    "revision": "bc769ad321184d3c6f58",
+    "url": "/my-website/static/js/2.1ba8f1f4.chunk.js"
+  },
+  {
+    "revision": "312687650ecf95cf2205ba748f37e6e4",
+    "url": "/my-website/static/js/2.1ba8f1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0fc8df14c5ec529e09",
+    "url": "/my-website/static/js/main.4ec99746.chunk.js"
+  },
+  {
+    "revision": "7d60f9870cc305540b02",
+    "url": "/my-website/static/js/runtime-main.4444a555.js"
+  },
+  {
+    "revision": "14b113574329f6898775182018d65f47",
+    "url": "/my-website/static/media/background.14b11357.jpg"
+  },
+  {
+    "revision": "9c916a05953d84ea6e64c551b3ef6a8e",
+    "url": "/my-website/static/media/classify_ships.9c916a05.png"
+  },
+  {
+    "revision": "e20baa6c488dc8d93c8057bedb3fe24d",
+    "url": "/my-website/static/media/cubsgame.e20baa6c.jpg"
+  },
+  {
+    "revision": "75bc029a19ae6d11fad0294fd12b1c1b",
+    "url": "/my-website/static/media/default_pic.75bc029a.jpg"
+  },
+  {
+    "revision": "2e779b259d6245d5cfdaa9280e4213c7",
+    "url": "/my-website/static/media/denver_main.2e779b25.jpg"
+  },
+  {
+    "revision": "7f1fcc4a5c1c4168a90015a11cd19650",
+    "url": "/my-website/static/media/guitarheropic.7f1fcc4a.png"
+  },
+  {
+    "revision": "4650f27061d3c27bc2da4f54dcac653d",
+    "url": "/my-website/static/media/me_and_merl.4650f270.jpg"
+  },
+  {
+    "revision": "d80a3f82e9b5e7f1ac9b199cb57f8948",
+    "url": "/my-website/static/media/mips_dis.d80a3f82.png"
+  },
+  {
+    "revision": "758aed4a1323f0bd9d904414186083e4",
+    "url": "/my-website/static/media/mywebsite.758aed4a.png"
+  },
+  {
+    "revision": "382209e0a3ddfcf75f8dfdca2a1d3902",
+    "url": "/my-website/static/media/newyears_boys.382209e0.jpg"
+  },
+  {
+    "revision": "52e73076e77220a7c4634d306749a3b6",
+    "url": "/my-website/static/media/pf_example.52e73076.png"
+  },
+  {
+    "revision": "0f4401fb924bd207a8f36f6dc862ed4e",
+    "url": "/my-website/static/media/plotscript_example.0f4401fb.png"
+  },
+  {
+    "revision": "1eab8e2ed74e5b143c667cd80238dd1a",
+    "url": "/my-website/static/media/yosemite_entrance.1eab8e2e.jpg"
+  },
+  {
+    "revision": "b01c5701148d7f10940f544508dab413",
+    "url": "/my-website/static/media/yosemite_halfdome.b01c5701.jpg"
+  },
+  {
+    "revision": "00ebc01a1659b369a47861c78b0f8870",
+    "url": "/my-website/static/media/yosemite_main.00ebc01a.jpg"
+  }
+]);
